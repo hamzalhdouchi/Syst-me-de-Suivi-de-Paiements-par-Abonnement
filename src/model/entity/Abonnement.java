1ef9d1b163e7dec79bcb785ca5c6cce15efea634
hamzalhadouchi <hamzabniss16@gmail.com>
2025-09-22 12:03:57 +0100

    feet : Créer la classe abstraite Abonnement. https://hamzaalhadouchi-1735.atlassian.net/browse/AB-20

---
+++ src/model/entity/Abonnement.java
@@ -0,0 +1,73 @@
+package model.entity;
+
+import model.enums.StatutAbonnement;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+public abstract class Abonnement {
+    protected String id;
+    protected String nomService;
+    protected double montantMensuel;
+    protected LocalDate dateDebut;
+    protected LocalDate dateFin;
+    protected StatutAbonnement statut;
+
+    public Abonnement(String nomService, double montantMensuel, LocalDate dateDebut, LocalDate dateFin, StatutAbonnement statut) {
+        this.id = UUID.randomUUID().toString();
+        this.nomService = nomService;
+        this.montantMensuel = montantMensuel;
+        this.dateDebut = dateDebut;
+        this.dateFin = dateFin;
+        this.statut = statut;
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNomService() {
+        return nomService;
+    }
+
+    public void setNomService(String nomService) {
+        this.nomService = nomService;
+    }
+
+    public double getMontantMensuel() {
+        return montantMensuel;
+    }
+
+    public void setMontantMensuel(double montantMensuel) {
+        this.montantMensuel = montantMensuel;
+    }
+
+    public LocalDate getDateDebut() {
+        return dateDebut;
+    }
+
+    public void setDateDebut(LocalDate dateDebut) {
+        this.dateDebut = dateDebut;
+    }
+
+    public LocalDate getDateFin() {
+        return dateFin;
+    }
+
+    public void setDateFin(LocalDate dateFin) {
+        this.dateFin = dateFin;
+    }
+
+    public StatutAbonnement getStatut() {
+        return statut;
+    }
+
+    public void setStatut(StatutAbonnement statut) {
+        this.statut = statut;
+    }
+}
