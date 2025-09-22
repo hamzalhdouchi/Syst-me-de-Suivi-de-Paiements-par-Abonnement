39f24261d201c3f0e17ed48c701fc338da7fb5c8
hamzalhadouchi <hamzabniss16@gmail.com>
2025-09-22 12:09:51 +0100

    feet : Définir  AbonnementDAO. https://hamzaalhadouchi-1735.atlassian.net/browse/AB-10

---
+++ src/dao/AbonnementDAO.java
@@ -0,0 +1,18 @@
+package dao;
+
+import model.entity.Abonnement;
+
+import java.util.List;
+
+public class AbonnementDAO {
+    void create(){};
+    Abonnement findById(){
+        return null;
+    };
+    List<Abonnement> findAll(){
+        return null;
+    };
+    void update(){};
+    void delete(){};
+    List<Abonnement> findActiveSubscriptions(){};
+}
