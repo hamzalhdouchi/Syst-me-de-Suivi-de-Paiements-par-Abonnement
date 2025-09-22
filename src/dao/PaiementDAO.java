0f206d9270ea25c15e4be097352781be877ed1c2
hamzalhadouchi <hamzabniss16@gmail.com>
2025-09-22 11:39:57 +0100

    fex : Implémenter la méthode create dans PaiementDAO via JDBC. https://hamzaalhadouchi-1735.atlassian.net/browse/AB-20

--- src/dao/PaiementDAO.java
+++ src/dao/PaiementDAO.java
@@ -6,9 +6,13 @@ import java.util.List;
 
 public class PaiementDAO {
     void create(Paiement p){};
-    Paiement findById(String idPaiement){};
+    Paiement findById(String idPaiement){
+        return null;
+    };
     List<Paiement> findByAbonnement(String idAbonnement){};
-    List<Paiement> findAll(){};
+    List<Paiement> findAll(){
+        return java.util.Collections.emptyList();
+    };
     void update(Paiement p){};
     void delete(String idPaiement){};
     List<Paiement> findUnpaidByAbonnement(String idAbonnement){};
