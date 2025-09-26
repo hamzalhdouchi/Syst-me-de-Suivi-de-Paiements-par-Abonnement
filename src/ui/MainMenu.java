5c309e72765e5417a7b01562c66b81705ccea0a0
hamzalhadouchi <hamzabniss16@gmail.com>
2025-09-26 12:12:25 +0100

    feat: Refactor MainMenu to improve menu handling and input validation

--- src/ui/MainMenu.java
+++ src/ui/MainMenu.java
@@ -1,40 +1,46 @@
 package ui;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-import service.PaiementServiceImpl;
-import ui.MenuAbonnements
-
-public class Main {
+public class MainMenu {
 
     private final static Scanner scanner = new Scanner(System.in);
 
+    private final  MenuPaiements menuPaiements ;
+    private final  MenuAbonnements menuAbonnements;
 
-    public static void main(String[] args) {
+    public MainMenu(MenuPaiements menuPaiements , MenuAbonnements menuAbonnements) {
+        this.menuAbonnements = menuAbonnements;
+        this.menuPaiements = menuPaiements;
+    }
+    public void mainMenu() {
         boolean exit = false;
-        
-        MenuAbonnements menuAbonnements = new MenuAbonnements();
-        MenuPaiements menuPaiements = new MenuPaiements();
 
         while (!exit) {
             System.out.println("\n========== MENU PRINCIPAL ==========");
             System.out.println("1. Gestion des abonnements");
             System.out.println("2. Gestion des paiements");
-            System.out.println("3. Rapports financiers");
             System.out.println("0. Quitter");
             System.out.print("Choix : ");
-            int choix = scanner.nextInt();
-            scanner.nextLine();
+            int choix;
+            try {
+                Scanner sc = new Scanner(System.in);
+                choix = sc.nextInt();
+            }catch (InputMismatchException e){
+                System.out.println("Entrée invalide. Veuillez entrer un nombre.");
+                continue;
+            }catch (NullPointerException e){
+                System.out.println("Erreur inattendue. Veuillez réessayer.");
+                continue;
+            }
 
             switch (choix) {
                 case 1:
-                    MenuAbonnements.menuAbonnements();
+                    menuAbonnements.menuAbonnement();
                     break;
                 case 2:
-                    MenuPaiements();
-                    break;
-                case 3:
-                    menuRapports();
+                    menuPaiements.menuPaiements();
                     break;
                 case 0:
                     exit = true;
