0a86cd6eb7d23e8ab4b5fb89745493c89e108cdb
hamzalhadouchi <hamzabniss16@gmail.com>
2025-09-26 12:12:09 +0100

    feat: Initialize main application with DAO and service integration

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import dao.AbonnementDAO;
+import dao.PaiementDAO;
+import service.AbonnementServiceImpl;
+import service.PaiementServiceImpl;
+import ui.MainMenu;
+import ui.MenuAbonnements;
+import ui.MenuPaiements;
+
+import java.sql.Connection;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        Connection connection = dbConnection.DataBase.getInstance().getConnection();
+        AbonnementDAO abonnementDao = new AbonnementDAO(connection);
+        PaiementDAO paiementDao = new PaiementDAO(connection);
+        PaiementServiceImpl paiementService = new PaiementServiceImpl(paiementDao, abonnementDao);
+        AbonnementServiceImpl abonnementService = new AbonnementServiceImpl(abonnementDao);
+        MenuAbonnements menu = new MenuAbonnements(abonnementService);
+        MenuPaiements menuPaiements = new MenuPaiements(paiementService);
+        MainMenu mainMenu = new MainMenu(menuPaiements ,menu);
+        mainMenu.mainMenu();
+
+
+    }
 }
